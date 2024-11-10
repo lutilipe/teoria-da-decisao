78ee5bb774e924865d6f401ec7c0c0971e80e9ae
lutilipe <lutilipe02@hotmail.com>
2024-11-09 22:09:14 -0300

    feat: add plots

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pandas as pd
-import copy
+import statistics
 from concurrent.futures import ProcessPoolExecutor
 import multiprocessing
+import matplotlib.pyplot as plt
+import networkx as nx
 
 """ 
 n = ativo
@@ -77,7 +79,7 @@ def penalizacao_restricoes(solution, p):
     return penalidade
 
 def f1(solution, p):
-    result = (p.d.values * solution.x.values @ solution.y.values @ solution.h.values.T).sum()
+    result = (p.d.values * solution.x.values).sum()
 
     solution.fitness = result
     solution.penalty = penalizacao_restricoes(solution, p)
@@ -130,15 +132,15 @@ def initialize_solution(p, apply_constructive_heuristic=False):
 
 def neighborhood_change(current_solution, new_solution, k):
     if new_solution.penalized_fitness < current_solution.penalized_fitness:
-        return copy.deepcopy(new_solution), 1
+        return new_solution, 1
     return current_solution, k + 1
 
 def local_search_first_improvement(solution, p):
-    current_solution = copy.deepcopy(solution)
+    current_solution = solution
 
 
 def GVNS(initial_solution, objective_function, p):
-    max_num_evaluations = 2000
+    max_num_evaluations = 10
     num_evaluations = 0
     k_max = 3
     
@@ -146,9 +148,9 @@ def GVNS(initial_solution, objective_function, p):
     new_solution = initial_solution
 
     history = Struct()
-    history.fit = []
-    history.pen = []
-    history.fit_pen = []
+    history.fitness = []
+    history.penalty = []
+    history.penalized_fitness = []
     
     while num_evaluations < max_num_evaluations:
         k = 1
@@ -161,9 +163,9 @@ def GVNS(initial_solution, objective_function, p):
 
             current_solution, k = neighborhood_change(current_solution, new_solution, k)
 
-            history.fit.append(current_solution.fitness)
-            history.pen.append(current_solution.penalty)
-            history.fit_pen.append(current_solution.penalized_fitness)
+            history.fitness.append(current_solution.fitness)
+            history.penalty.append(current_solution.penalty)
+            history.penalized_fitness.append(current_solution.penalized_fitness)
 
             print(num_evaluations)
             
@@ -174,8 +176,7 @@ def optimize_instance(objective_function, p):
     best_solution, history = GVNS(initial_solution, objective_function, p)
     return best_solution, history
 
-def optimize(objective_function):
-    p = probdef()
+def optimize(p, objective_function):
     results = []
 
     num_instances = 5 
@@ -185,4 +186,112 @@ def optimize(objective_function):
         results = [future.result() for future in futures]
     return results
 
-print(optimize(f1))
+def get_metrics(results):
+    max_sol = None
+    min_sol = None
+    std_dev = statistics.stdev([x[0].penalized_fitness for x in results])
+
+    for solution, _ in results:
+        if (max_sol == None and min_sol == None):
+            max_sol = min_sol = solution
+            continue
+
+        if solution.penalized_fitness > max_sol.penalized_fitness:
+            max_sol = solution
+
+        if solution.penalized_fitness < min_sol.penalized_fitness:
+            min_sol = solution
+
+    return std_dev, max_sol, min_sol
+
+def plot_convergence(results, fobj):
+    histories = [x[1] for x in results]
+    plt.figure(figsize=(10, 6))
+    for history in histories:
+        s = len(history.penalized_fitness)
+        plt.plot(np.linspace(0, s-1, s), history.penalized_fitness, 'k-', alpha=0.7)
+
+    plt.title(f'Evolução da qualidade da solução candidata - {fobj}')
+    plt.ylabel('fitness(x) penalizado')
+    plt.xlabel('Número de avaliações')
+    plt.grid(True)
+    
+    plt.savefig(f"ev_{fobj}.png", format='png', dpi=300, bbox_inches='tight')
+
+def visualize_network(p, solution, fobj):
+    G = nx.Graph()
+    
+    for base in p.m:
+        is_occupied = False
+        for team in p.s:
+            if solution.y.loc[base, team] == 1:
+                is_occupied = True
+                break
+        node_type = "base" if not is_occupied else "active_base"
+        G.add_node(base, node_type=node_type, status='available')
+        
+
+    for ativo in p.n:
+        G.add_node(ativo, node_type='ativo')
+    
+    team_colors = {
+        team: color for team, color in zip(p.s, ['#1f78b4', '#33a02c', '#e31a1c'])
+    }
+    
+    edge_colors = []
+    edge_labels = {}
+    
+    for ativo in p.n:
+        for base in p.m:
+            if solution.x.loc[ativo, base] == 1:
+                team_responsible = None
+                for team in p.s:
+                    if solution.h.loc[ativo, team] == 1:
+                        team_responsible = team
+                        break
+                if team_responsible is not None:
+                    G.add_edge(base, ativo, node_type=team_responsible)
+                    edge_colors.append(team_colors[team_responsible])
+                    edge_labels[(base, ativo)] = team_responsible
+    
+    plt.figure(figsize=(10, 7))
+    
+    pos = {node: node for node in G.nodes}
+    
+    base_nodes = [node for node, data in G.nodes(data=True) if data['node_type'] == 'base']
+    active_base_nodes = [node for node, data in G.nodes(data=True) if data['node_type'] == 'active_base']
+    ativo_nodes = [node for node, data in G.nodes(data=True) if data['node_type'] == 'ativo']
+
+    nx.draw_networkx_nodes(G, pos, nodelist=base_nodes, node_color='#66c2a5', node_shape='s', 
+                           label='Base Disponível', node_size=100, edgecolors='black', linewidths=1.5)
+    nx.draw_networkx_nodes(G, pos, nodelist=active_base_nodes, node_color='#fc8d62', node_shape='D', 
+                           label='Base Ocupada', node_size=100, edgecolors='black', linewidths=1.5)
+    nx.draw_networkx_nodes(G, pos, nodelist=ativo_nodes, node_color='#8da0cb', node_shape='o', 
+                           label='Ativo', node_size=50, edgecolors='black', linewidths=1)
+    
+    edges = list(G.edges())
+    nx.draw_networkx_edges(G, pos, edgelist=edges, edge_color=edge_colors, width=1.5, alpha=0.7, style="dashed")
+
+    for team, color in team_colors.items():
+        plt.plot([], [], color=color, label=f'Equipe {team}', linewidth=2, linestyle='--')
+    
+    plt.legend(scatterpoints=1, markerscale=1, loc='upper left', 
+               bbox_to_anchor=(1.05, 1), frameon=True, fancybox=True, shadow=True, 
+               borderpad=1.2, fontsize=10)
+    
+    plt.axis('off')
+    plt.grid(False)
+    
+    plt.savefig(f"network_{fobj}.png", format='png', dpi=300, bbox_inches='tight')
+
+p = probdef()
+
+results_f1 = optimize(p, f1)
+std_dev_f1, max_sol_f1, min_sol_f1 = get_metrics(results_f1)
+
+print(f"Desvio Padrao - F1: {std_dev_f1}")
+print(f"Maximo - F1: {max_sol_f1.penalized_fitness}")
+print(f"Minimo - F1: {min_sol_f1.penalized_fitness}")
+
+plot_convergence(results_f1, "F1")
+visualize_network(p, min_sol_f1, "F1")
